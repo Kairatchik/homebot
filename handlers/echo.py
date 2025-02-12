bd48ca3464cd198a298eaf71130e1edb7ff57b17
Kairatchik <tashbolotov07@gmail.com>
2025-02-12 17:20:03 +0600

    homework2

--- handlers/echo.py
+++ handlers/echo.py
@@ -1,10 +1,14 @@
 # echo.py
 from aiogram import types, Dispatcher
-
+import random
 
 async def echo_handler(message: types.Message):
-    await message.answer(message.text)
-
+    if message.text.lower() == "game":
+        games = ['⚽', '🎰', '🏀', '🎯', '🎳', '🎲']
+        game = random.choice(games)
+        await message.answer_dice(emoji=game)
+    else:
+        await message.answer(message.text)
 
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(echo_handler)
